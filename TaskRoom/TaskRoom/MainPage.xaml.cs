f318e5fa4acb5e44d359da0c13ac8d340f2143df
Aplefen1 <48839091+Aplefen1@users.noreply.github.com>
2019-11-19 10:46:12 +0000

    Created a menu page and started formatting

--- TaskRoom/TaskRoom/MainPage.xaml.cs
+++ TaskRoom/TaskRoom/MainPage.xaml.cs
@@ -7,11 +7,63 @@ using Xamarin.Forms;
 
 namespace TaskRoom
 {
+
     public partial class MainPage : ContentPage
     {
+        
         public MainPage()
         {
-            InitializeComponent();
+            //List<string> Buttons = new List<string>;
+            //Buttons = ["Play Game", "Leaderboards", "Settings", "Login"]
+
+            Padding = new Thickness(30);
+            
+
+            Content = new Frame
+            {
+                BorderColor = Color.White,
+                BackgroundColor = Color.FromRgb(204, 230, 255),
+                //HorizontalOptions = LayoutOptions.CenterAndExpand,
+                //VerticalOptions = LayoutOptions.StartAndExpand,
+
+                Content = new StackLayout
+                {
+                    Children = {
+                        new Label
+                        {
+                            Text = "Welcome To task Room!!",
+                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                            VerticalOptions = LayoutOptions.Start,
+                        },
+                        new Button
+                        {
+                            Text = "Tasks",
+                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) * 2,
+                            VerticalOptions = LayoutOptions.CenterAndExpand,
+                        },
+                        new Button
+                        {
+                            Text = "Leaderboards",
+                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) *1.4,
+                            VerticalOptions = LayoutOptions.CenterAndExpand,
+                        },
+                        new Button
+                        {
+                            Text = "Settings",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            VerticalOptions = LayoutOptions.End,
+                            HorizontalOptions = LayoutOptions.CenterAndExpand,
+                        },
+                        new Button
+                        {
+                            Text = "Login",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            VerticalOptions = LayoutOptions.End,
+                            HorizontalOptions = LayoutOptions.End
+                        },
+                    }
+                }
+            };
         }
     }
 }
