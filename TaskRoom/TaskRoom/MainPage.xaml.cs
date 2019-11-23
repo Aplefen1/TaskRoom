d33a3d9286fc37157c4f1b2dd55b06463aedaa1d
Aplefen1 <48839091+Aplefen1@users.noreply.github.com>
2019-11-23 12:20:55 +0000

    Tidied up code

--- TaskRoom/TaskRoom/MainPage.xaml.cs
+++ TaskRoom/TaskRoom/MainPage.xaml.cs
@@ -10,60 +10,82 @@ namespace TaskRoom
 
     public partial class MainPage : ContentPage
     {
-        
         public MainPage()
         {
             //List<string> Buttons = new List<string>;
             //Buttons = ["Play Game", "Leaderboards", "Settings", "Login"]
 
             Padding = new Thickness(30);
-            
 
-            Content = new Frame
+
+            //Elements in the stack
+            Button Settings = new Button
+            {
+                Text = "Settings",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                VerticalOptions = LayoutOptions.End,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            Button Leaderboards = new Button
+            {
+                Text = "Leaderboards",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) * 1.4,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            Button Tasks = new Button
+            {
+                Text = "Tasks",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) * 2,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            Button Login = new Button
+            {
+                Text = "Login",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                VerticalOptions = LayoutOptions.End,
+                HorizontalOptions = LayoutOptions.End
+            };
+
+            Label Welcome = new Label
+            {
+                Text = "Welcome To task Room!!",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.Start,
+            };
+
+            var Buttons = new List<Button> { Tasks, Leaderboards, Settings, Login };
+
+
+            //End of elements in the stack (Move this to a namespace or list to enumerate through the items??
+
+            StackLayout MainStack = new StackLayout();
+
+            StackLayout StackA = new StackLayout();
+            
+            Frame container = new Frame
             {
                 BorderColor = Color.White,
                 BackgroundColor = Color.FromRgb(204, 230, 255),
-                //HorizontalOptions = LayoutOptions.CenterAndExpand,
-                //VerticalOptions = LayoutOptions.StartAndExpand,
-
-                Content = new StackLayout
-                {
-                    Children = {
-                        new Label
-                        {
-                            Text = "Welcome To task Room!!",
-                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                            VerticalOptions = LayoutOptions.Start,
-                        },
-                        new Button
-                        {
-                            Text = "Tasks",
-                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) * 2,
-                            VerticalOptions = LayoutOptions.CenterAndExpand,
-                        },
-                        new Button
-                        {
-                            Text = "Leaderboards",
-                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) *1.4,
-                            VerticalOptions = LayoutOptions.CenterAndExpand,
-                        },
-                        new Button
-                        {
-                            Text = "Settings",
-                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                            VerticalOptions = LayoutOptions.End,
-                            HorizontalOptions = LayoutOptions.CenterAndExpand,
-                        },
-                        new Button
-                        {
-                            Text = "Login",
-                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                            VerticalOptions = LayoutOptions.End,
-                            HorizontalOptions = LayoutOptions.End
-                        },
-                    }
-                }
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.StartAndExpand  
             };
+
+            StackA.Children.Add(Welcome);
+            foreach (Button Butt in Buttons)
+            {
+                StackA.Children.Add(Butt);
+            };
+
+            container.Content = StackA;
+            MainStack.Children.Add(container);
+
+            Content = MainStack;
+
+            //InitializeComponent();
         }
     }
+
 }
